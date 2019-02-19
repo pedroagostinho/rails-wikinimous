40083e77c81e58dc922e9bf9397639d3e1e15d43
Pedro Agostinho <pedroagosti@gmail.com>
2019-02-19 18:13:17 +0000

    Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  article = Article.new(
+    title: Faker::RickAndMorty.quote,
+    content: Faker::Lorem.paragraph(3)
+  )
+  article.save!
+end
